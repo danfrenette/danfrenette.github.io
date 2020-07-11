9f3bfa135cda62294ce3fb4e8736b35bedccf617
danfrenette <dan.r.frenette@gmail.com>
2020-07-11 18:12:27 -0400

    HD background now fades in over low-res one
    I'm using a blurry, quick-loading version of the background image to
    start, while also loading the original HD-version of the image in a
    totally transparent image tag. Once the image has loaded, a callback
    function is triggered to fade the image in over the blurry one with CSS
    transitions.
    
    Pattern for this inspired from this: https://github.com/BojanGvozderac/react-iron-image

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import headshot from "../../images/headshot.jpg";
+import nhBackround from "../../images/nhbackground.jpg";
 import { Bio } from "../Bio";
 import { Posts } from "../Posts";
 
 export const Content = () => {
   const [displayPosts, setDisplayPosts] = useState(false);
+  const [imageClass, setImageClass] = useState("loading");
+  const imageLoadHandler = () => { setImageClass("loaded") }
 
   return (
     <div className="main">
+      <img
+        src={nhBackround}
+        alt="NH Forest/Waterfall (full resolution)"
+        className={`background-image ${imageClass}`}
+        onLoad={imageLoadHandler}
+      />
       <div className="text-wrapper">
         <button
           className="headshot-wrapper"
